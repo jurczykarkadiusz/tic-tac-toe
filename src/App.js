2714f6175785e71c6e48919a6d5298287688e8e5
jurczykarkadiusz <jurczykarkadiusz@gmail.com>
2023-08-31 16:49:51 +0200

    plansza ready

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
+import { useState } from "react"
+import Cell from "./components/Cell"
+
 const App = () => {
+  const [cells, setCells] = useState(["", "", "", "", "", "", "", "", "", ])
+  
   return (
     <div className="app">
+      <div className="gameboard">
+      {cells.map((cell, index) => <Cell key={index} id={index} cell={cell}/>)}
+      </div>
+      <p></p>
     </div>
   )
 }
