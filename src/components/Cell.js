a46e9a245461ef1b8c294c9e0f77110bfc7a6799
jurczykarkadiusz <jurczykarkadiusz@gmail.com>
2023-08-31 20:05:05 +0200

    chyba tyle

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,4 +1,4 @@
-const Cell = ({ id, cell, setCells, go, setGo }) => {
+const Cell = ({ id, cell, setCells, go, setGo, cells, winningMessage }) => {
     
     const handleClick = (e) => {
         const taken = e.target.firstChild.classList.contains("circle") ||
@@ -7,18 +7,31 @@ const Cell = ({ id, cell, setCells, go, setGo }) => {
         if (!taken) {
             if (go === "circle") {
                 e.target.firstChild.classList.add("circle")
+                handleCellChange("circle")
                 setGo("cross")
             }
             if (go === "cross") {
                 e.target.firstChild.classList.add("cross")
+                handleCellChange("cross")
                 setGo("circle")
             }
         }
 
     }
 
+    const handleCellChange = (className) => {
+        const nextCells = cells.map((cell, index) => {
+            if (index === id) {
+                return className
+            } else {
+                return cell
+            }
+        })
+        setCells(nextCells )
+    }
+
     return (
-        <div className="square" id={id} onClick={handleClick}>
+        <div className="square" id={id} onClick={!winningMessage && handleClick}>
             <div className={cell}></div>
         </div>
     )
